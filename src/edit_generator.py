bf810c335350f56edf0bd6943cbcede3fe3423f3
twoc0bb <pixelmaker062@gmail.com>
2023-02-28 19:57:50 +1030

    implemented audio overlay and music lengths

--- src/edit_generator.py
+++ src/edit_generator.py
@@ -1,21 +1,37 @@
 import json
 from pytube import YouTube
 from youtubesearchpython import VideosSearch
+from moviepy import editor
+
+MUSIC = ['tpgi.mp4', 18.0]
+
+VID_TIME = MUSIC[1] + 10
 
 def YTDownload(link):
-    ytObject = YouTube(link)
+    ytObject = YouTube("what is {}".format(link))
     ytObject = ytObject.streams.get_highest_resolution()
     
     try:
-        ytObject.download()
-    except Exception as e:
-        print("Error downloading '{}\n{}'".format(link, e.message))
+        ytObject.download(filename="download.mp4")
+    except:
+        print("Error downloading '{}'".format(link))
     
 def YTSearch(term):
-	videosSearch = VideosSearch('what is {}'.format(term), limit = 1)
-        
+	videosSearch = VideosSearch('{}'.format(term), limit = 1)
+    
 	return videosSearch.result()["result"][0]["link"]
 
 
-YTDownload(YTSearch('francium'))
+YTDownload(YTSearch('rubidium'))
 
+# Perfect Girl Instrumental means the video should cut at ~18 seconds
+
+clip = editor.VideoFileClip("download.mp4")
+clip = clip.subclip(0.0, VID_TIME)
+
+audioclip = editor.AudioFileClip("tpgi.mp4")
+audioclip = audioclip.subclip(0.0, VID_TIME)
+
+new_audioclip = editor.CompositeAudioClip([clip.audio, audioclip])
+clip.audio = new_audioclip
+clip.write_videofile("new_clip.mp4")
