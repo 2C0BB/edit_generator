a0cfc2ff10cd8ff111665e6ca6f192390d38bbbb
twoc0bb <pixelmaker062@gmail.com>
2023-03-03 10:56:25 +1030

    dev

--- src/img_vid.py
+++ src/img_vid.py
@@ -4,45 +4,84 @@ from moviepy import editor
 import wget
 from youtubesearchpython import VideosSearch
 import os
-import glob
 from PIL import Image
 from moviepy.video.fx.all import crop
+import bs4 as bs
+import requests
 
-def imgs2Vid(term):
+# def YTImgs2Vid(term, amount):
     
 
-	videosSearch = VideosSearch('{}'.format(term), limit = 8)
+# 	videosSearch = VideosSearch('{}'.format(term), limit = amount)
 
-	filenames = []
+# 	filenames = []
     
-	for idx, url in enumerate(videosSearch.result()["result"]):
-		url = url["thumbnails"][0]["url"]
+# 	for idx, url in enumerate(videosSearch.result()["result"]):
+# 		url = url["thumbnails"][0]["url"]
 
-		filenames.append(download_png(url, str(idx)))
-              
-	#filenames  = ["./frames/"+x for x in filenames]
+# 		filenames.append(download_png(url, str(idx)))
+
+# 	clips = [editor.ImageClip(m).set_duration(1) for m in filenames]
+# 	clips = [c.resize(5) for c in clips] # maybe crop here
+
+# 	concat_clip = editor.concatenate_videoclips(clips, method="compose")
+	
+# 	return concat_clip
+
+def GoogleImgs2Vid(term, amount):
+	r = requests.get("https://www.bing.com/images/search?q={}".format(term), verify=False)
+
+	if r.status_code != 200:
+		raise Exception("couldn't get google images")
+	
+	source = r.text
+	soup = bs.BeautifulSoup(source, 'html')
+
+	imgs = soup.find_all('img')
+
+	filt_imgs = []
+	for i in imgs:
+		try:
+			s = imgs['src']
+			if s.startswith("https://"):
+				filt_imgs.append(s)
+		except:
+			continue
+
+	filenames = download_list(filt_imgs)
+
+	# concat the clips together
 
 	clips = [editor.ImageClip(m).set_duration(1) for m in filenames]
-	clips = [crop(c, x1=100, width=1080).resize(5) for c in clips]
+	clips = [c.resize(5) for c in clips]
 
-	concat_clip = editor.concatenate_videoclips(clips, method="compose")
-	
-	return concat_clip
+	return editor.concatenate_videoclips(clips, method="compose")
 
+
+
+def download_list(urls):
+	filenames = []
+
+	for idx, url in enumerate(urls):
+		filenames.append(download_png(url, str(idx)))
+
+	return filenames
 
 def download_png(url, name):
+	print(url)
+	#return
 	file_n = wget.download(url, out=F_PATH)
 
-	ext = "." + file_n.split(".")[-1]
-	new_name = F_PATH + name + ext
+	# ext = "." + file_n.split(".")[-1]
+	# new_name = F_PATH + name + ext
 
-	if os.path.exists(new_name):
-		os.remove(new_name)
-	os.rename(file_n, new_name)
+	# if os.path.exists(new_name):
+	# 	os.remove(new_name)
+	# os.rename(file_n, new_name)
 
-	im = Image.open(new_name)
+	im = Image.open(file_n)
 
-	png_name = F_PATH + name + ".png"
+	png_name = file_n.split(".")[0]+ ".png"
 	im.save(png_name)
 
 	return png_name
--- src/vid_gen.py
+++ src/vid_gen.py
@@ -1,6 +1,6 @@
 import yt
 from constants import *
-from img_vid import imgs2Vid
+from img_vid import GoogleImgs2Vid
 
 from moviepy import editor
 from moviepy.video.fx.all import crop
@@ -16,22 +16,20 @@ def generate_video(vid_term, img_term, music):
 	# get downloaded video as an object and clip it to the time before the beat drop
 	clip = editor.VideoFileClip(B_PATH+"download.mp4")
 	clip = clip.subclip(0.0, 15.0)
-	clip = crop(clip, x1=100, width=1080)
+	#clip = crop(clip, x1=100, width=1080)
 	clip = clip.resize(2)
 
 	# add the video of the images to the end of the clip, compose means black bars
-	clip = editor.concatenate_videoclips([clip, imgs2Vid(img_term)], method="compose")
+	clip = editor.concatenate_videoclips([clip, GoogleImgs2Vid(img_term, 8)], method="compose")
 
 	# get the audio clip and clip it down so that it runs for 10s after beat drop
-	#print(music[0])
 	audioclip = editor.AudioFileClip(music)
-	#audioclip = audioclip.subclip(music[1], vid_time)
 
 	# create a new audio clip which overlays the original vid audio and the edit audio
 	new_audioclip = editor.CompositeAudioClip([clip.audio, audioclip])
     # set the clip's audio to the new composited audio
 	clip.audio = new_audioclip
 
-	# save the clip to a file
+	# save the clip to the output folder as "new_clip.mp4"
 	clip.write_videofile(B_PATH+"new_clip.mp4", fps=24)
 
